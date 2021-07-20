56fbe13ebf29da75b027514648212e180ecd477b
quesoo <quesoo@users.noreply.github.com>
2021-07-20 13:34:10 +0200

    RMX1805: add permissions for torch/flash

--- device.mk
+++ device.mk
@@ -1,3 +1,4 @@
+  
 #
 # Copyright (C) 2020 The LineageOS Project
 #
@@ -187,6 +188,10 @@ PRODUCT_BOOT_JARS += \
 # TextClassifier
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
+
+# Torch
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml
 
 # Wifi
 PRODUCT_COPY_FILES += \
