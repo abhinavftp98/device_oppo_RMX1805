41462c92b9f031af2571b612a121e843ff2ea241
JamieHoSzeYui <JamieHoSzeYui@users.noreply.github.com>
2021-09-28 16:20:08 +0530

    RMX1805: remove old version gsounds

--- GoogleSounds/Android.mk
+++ GoogleSounds/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := GoogleSounds
 LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := GoogleSounds.apk
+LOCAL_SRC_FILES := PrebuiltGoogleSounds/PrebuiltGoogleSounds.apk
 LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
