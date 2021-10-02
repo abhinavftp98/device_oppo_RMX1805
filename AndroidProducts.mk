973b6e0900d977f36ed6eee6f1b8aee9d0b7aa5b
abhinavftp98 <abhishekftp99@gmailcom>
2021-10-02 12:19:03 +0530

    RMX1805: bringup for CherishOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_RMX1805.mk
+    $(LOCAL_DIR)/cherish_RMX1805.mk
 
+COMMON_LUNCH_CHOICES := \
+    cherish_RMX1805-user \
+    cherish_RMX1805-userdebug \
+    cherish_RMX1805-eng
--- cherish_RMX1805.mk
+++ cherish_RMX1805.mk
@@ -3,7 +3,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common LOS stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
 # Inherit from RMX1805 device
 $(call inherit-product, device/oppo/RMX1805/device.mk)
@@ -14,10 +14,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 PRODUCT_BRAND := oppo
 PRODUCT_DEVICE := RMX1805
 PRODUCT_MANUFACTURER := oppo
-PRODUCT_NAME := aosp_RMX1805
+PRODUCT_NAME := cherish_RMX1805
 PRODUCT_MODEL := Realme 2/C1
 
-# PRODUCT_GMS_CLIENTID_BASE := android-oppo
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
 TARGET_VENDOR := oppo
 TARGET_VENDOR_PRODUCT_NAME := RMX1805
 
@@ -27,6 +27,12 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 TARGET_GAPPS_ARCH :=arm64
 TARGET_BOOT_ANIMATION_RES := 720
 
-# Blaster
-TARGET_USES_FACE_UNLOCK := true
-BLASTER_BUILD_TYPE := OFFICIAL
+#CHERISH STAFF
+CHERISH_BUILD_TYPE := OFFICIAL
+WITH_GMS := true
+#USE_LAWNCHAIR :=true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_USES_BLUR := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
